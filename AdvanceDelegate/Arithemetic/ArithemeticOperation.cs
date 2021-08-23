e78f60a667a9bf6d35f20755c76c5517365eddea
maniram.yadav@taazaa.com <maniramg90@gmail.com>
2021-08-23 20:45:13 +0530

    initial commit

---
+++ AdvanceDelegate/Arithemetic/ArithemeticOperation.cs
@@ -0,0 +1,19 @@
+using System;
+namespace AdvanceDelegate.Arithemetic
+{
+    public class ArithemeticOperation
+    {
+        /* public static void Addition(double no1,double no2)
+        {
+            Console.WriteLine(no1+no2);
+        }
+ */       /*  public static void Multiply(double no1,double no2)
+        {
+            Console.WriteLine(no1*no2);
+        } */
+        /* public static void Divide(double no1,double no2)
+        {
+            Console.WriteLine(no1/no2);
+        } */
+    }
+}
---
+++ AdvanceDelegate/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using AdvanceDelegate.Arithemetic;
+delegate void ArithemeticDelegate(double D1,double D2);
+namespace AdvanceDelegate
+{
+    class Program
+    {
+        static void Main()
+        {
+            /* ArithemeticDelegate obj=new ArithemeticDelegate(ArithemeticOperation.Addition);
+            // obj.Invoke(3,6); // invoke Fun will  call Addtion Fun
+            obj+=ArithemeticOperation.Multiply;
+            obj.Invoke(9,6); */ 
+
+
+            // Anonyumous
+            ArithemeticDelegate mydel=(double d1,double d2)=> // lambda Expression
+            {
+                Console.WriteLine(d1*d2);
+            };
+            ArithemeticDelegate dive=delegate(double d1,double d2) // Anonymous Function
+            {
+                Console.WriteLine(d1/d2);
+            };
+            mydel(10,5);
+            dive(10,2);
+    
+        }
+    }
+}
