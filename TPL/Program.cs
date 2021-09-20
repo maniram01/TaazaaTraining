72b1e1acecd1aac8f81ed70f0f02dd55b436ba15
maniram01 <89043593+maniram01@users.noreply.github.com>
2021-09-20 10:53:18 +0530

    Add files via upload

---
+++ TPL/Program.cs
@@ -0,0 +1,26 @@
+﻿using System.Threading.Tasks;
+using System.Threading;
+using System;
+
+namespace TPL
+{
+    class MyApp
+    {
+        public static void longTask()  // as DbContext
+        {
+           Thread.Sleep(20000);  // sleep for 2s
+        } 
+        public async static void MyCall() // Repository   await and aync are in pair
+        {
+          await Task.Run(new Action(longTask)); //task ke ander delegate hai
+          System.Console.WriteLine("At this point new thread has been created ");
+        }
+         public static void Main()  // act as controller
+        {
+           MyCall();
+           System.Console.WriteLine("I am main thread");
+           Console.ReadLine();
+           
+        }
+    }
+}
